215b5c33476b6d9a12b55bd762657acc402df440
Michal Werner <michal@hurtowniapixeli.pl>
2016-10-23 00:39:33 +0200

    Fix babel-project-relative-import deprecation

--- projectTemplate/main.js
+++ projectTemplate/main.js
@@ -4,7 +4,8 @@ global.__SERVER__ = true
 global.__CLIENT__ = false
 
 require('babel-register')({
-  presets: ['react', 'es2015', 'stage-0']
+  presets: ['react', 'es2015', 'stage-0'],
+  sourceRoot: process.cwd()
 })
 require.extensions['.scss'] = function() { return }
 
@@ -12,7 +13,6 @@ var WebpackIsomorphicTools = require('webpack-isomorphic-tools')
 
 global.webpack_isomorphic_tools = new WebpackIsomorphicTools(
   require('./webpack-isomorphic-tools-configuration'))
-    .development(process.env.NODE_ENV !== 'production')
     .server(__dirname, function() {
       require('pixelcms-client/lib/server').default(
         process.env.NODE_ENV === 'production' ? require('./webpack.config.prod') : require('./webpack.config.dev'),
