d8ec3a1f531f8abf1b9c26ea0a47e10529e730b2
Michal Werner <michal@hurtowniapixeli.pl>
2016-10-27 22:38:19 +0200

    Removed unneeded code

--- src/actions/auth/logout.js
+++ src/actions/auth/logout.js
@@ -1,14 +1,10 @@
-// import browserHistory from 'react-router/lib/browserHistory'
-
 import ApiRequest from '~/utils/ApiRequest'
-import langPrefix from '~/utils/langPrefix'
 import addToast from '~/actions/toaster/addToast'
 
 const logout = () => (dispatch, getState) => {
   return new ApiRequest().delete('accounts/auth-info/', dispatch, getState)
     .then(
       (res) => {
-        // browserHistory.push(langPrefix('/', getState().route.lang))
         dispatch(addToast('success', res.body.msg, null))
       }
     )
