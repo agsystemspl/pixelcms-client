8e4eefd94a96f0e4e5091de60a5ec596a0019416
Michal Werner <michal@hurtowniapixeli.pl>
2016-11-15 16:53:53 +0100

    Adds missing exports

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ export Mailto from './components/utils/Mailto'
 export Breadcrumbs from './components/utils/Breadcrumbs'
 export Lightbox from './components/utils/Lightbox'
 export ContactForm from './components/utils/ContactForm'
+export FormHoneypot from './components/utils/FormHoneypot'
 export AuthLinks from './components/utils/AuthLinks'
 export Toaster from './components/utils/Toaster'
 export Loading from './components/utils/Loading'
@@ -45,6 +46,7 @@ export AdminLink from './components/liveAdmin/AdminLink'
 
 // Utils
 export apiRequest from './utils/apiRequest'
+export validateFormHoneypot from './utils/validateFormHoneypot'
 
 // Actions
 export * as actions from './actions'
