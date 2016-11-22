ed4834c58e7510d2b995d25d0ca6ea312ec0714f
Michal Werner <michal@hurtowniapixeli.pl>
2016-11-22 15:58:23 +0100

    showFirstLast and showPrevNext props in Pagination component

--- src/components/utils/Pagination.js
+++ src/components/utils/Pagination.js
@@ -20,50 +20,66 @@ const Pagination = props => {
       )
     }
   })
-  let prev
-  if (props.currentPage > 1) {
-    prev = (
-      <div className="prev">
-        <Link to={`?page=${props.currentPage - 1}`}>
-          <span><T t="Previous" /></span>
+  let prev, next
+  if (props.showPrevNext) {
+    if (props.currentPage > 1) {
+      prev = (
+        <div className="prev">
+          <Link to={`?page=${props.currentPage - 1}`}>
+            <span><T t="Previous" /></span>
+          </Link>
+        </div>
+      )
+    }
+    if (props.currentPage < props.numPages) {
+      next = (
+        <div className="next">
+          <Link to={`?page=${props.currentPage + 1}`}>
+            <span><T t="Next" /></span>
+          </Link>
+        </div>
+      )
+    }
+  }
+  let first, last
+  if (props.showFirstLast) {
+    first = (
+      <div className="first">
+        <Link to="?page=1">
+          <span><T t="First" /></span>
         </Link>
       </div>
     )
-  }
-  let next
-  if (props.currentPage < props.numPages) {
-    next = (
-      <div className="next">
-        <Link to={`?page=${props.currentPage + 1}`}>
-          <span><T t="Next" /></span>
+    last = (
+      <div className="last">
+        <Link to={`?page=${props.numPages}`}>
+          <span><T t="Last" /></span>
         </Link>
       </div>
     )
   }
   return (
     <div className="pagination">
-      <div className="first">
-        <Link to="?page=1">
-          <span><T t="First" /></span>
-        </Link>
-      </div>
+      {first}
       {prev}
       <div className="pages">
         {pages}
       </div>
       {next}
-      <div className="last">
-        <Link to={`?page=${props.numPages}`}>
-          <span><T t="Last" /></span>
-        </Link>
-      </div>
+      {last}
     </div>
   )
+}
+Pagination.defaultProps = {
+  showFirstLast: true,
+  showPrevNext: true
 }
 Pagination.propTypes = {
   count: PropTypes.number.isRequired,
   numPages: PropTypes.number.isRequired,
-  currentPage: PropTypes.number.isRequired
+  currentPage: PropTypes.number.isRequired,
+  showFirstLast: PropTypes.bool.isRequired,
+  showPrevNext: PropTypes.bool.isRequired
 }
 
 export default Pagination
