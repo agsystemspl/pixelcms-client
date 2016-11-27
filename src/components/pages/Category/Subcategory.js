500b4f186329f4be48d64a0791b1d8a73876cfa5
Michal Werner <michal@hurtowniapixeli.pl>
2016-11-27 20:32:54 +0100

    Subcategories images and descriptions

--- src/components/pages/Category/Subcategory.js
+++ src/components/pages/Category/Subcategory.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 
 import Link from '~/components/utils/Link'
+import EditableContent from '~/components/liveAdmin/EditableContent'
 import AdminLink from '~/components/liveAdmin/AdminLink'
 
 const Subcategory = props => (
@@ -11,12 +12,34 @@ const Subcategory = props => (
         {props.name}
       </Link>
     </h2>
+    {props.image && (
+      <div className="image">
+        <Link to={props.route}>
+          <img
+            src={props.image}
+            alt=""
+          />
+        </Link>
+      </div>
+    )}
+    {props.description && (
+      <div className="description">
+        <EditableContent
+          model="cms.content.models.Category"
+          pk={props.pk}
+          field="description"
+          content={props.description}
+        />
+      </div>
+    )}
   </div>
 )
 Subcategory.propTypes = {
   pk: PropTypes.number.isRequired,
   route: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  image: PropTypes.string
 }
 
 export default Subcategory
